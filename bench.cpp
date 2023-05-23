220dfc238cc2c2ce93f270e6a7e52a7c1dfeffb6
angsch <17718454+angsch@users.noreply.github.com>
2023-05-23 21:25:13 +0200

    Refactor bench

--- bench.cpp
+++ bench.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <complex>
+#include <fstream>
 #include <initializer_list>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 
@@ -18,7 +20,7 @@ template<typename Prec>
 microseconds bench(kernel_t variant, char uplo, char trans, char diag, int n,
     const Prec *__restrict__ A, int ldA, Prec *__restrict__ x, int incx)
 {
-    void (*trsv_var[])(char, char, char, int, const double *__restrict__ , int, double *__restrict__) = {
+    void (*trsv_var[])(char, char, char, int, const Prec *__restrict__ , int, Prec *__restrict__) = {
         trsv_selector<Prec, USE_BLAS_CALL>,
         trsv_selector<Prec, UNROLL_1>,
         trsv_selector<Prec, UNROLL_2>
@@ -31,42 +33,72 @@ microseconds bench(kernel_t variant, char uplo, char trans, char diag, int n,
     std::cout << "  Time elapsed = " << duration.count() << " µs" << std::endl;
     return duration;
 }
-} // namespace
 
-
-int main(int argc, char **argv)
+template<typename Prec>
+void bench(const std::string &output_filename)
 {
+    std::ofstream out;
+    out.open(output_filename);
+
+    // Write header. (As of C++20, std::format can be used)
+    out << std::setw(13) << std::left << "Kernel"
+        << std::setw( 6) << std::left << "uplo"
+        << std::setw( 6) << std::left << "diag"
+        << std::setw( 6) << std::left << "trans"
+        << std::setw( 8) << std::left << "n"
+        << std::setw(14) << std::left << "duration [µs]"
+        << std::endl;
+
+    using Real = remove_complex_t<Prec>;
     constexpr int maxn = 4000;
     int ldA = maxn;
 
-    //typedef std::complex<double> prec_t;
-    typedef double prec_t;
-
-    std::vector<prec_t> x = std::vector<prec_t>(maxn);
-    std::vector<prec_t> A = std::vector<prec_t>(maxn * ldA);
+    std::vector<Prec> x = std::vector<Prec>(maxn);
+    std::vector<Prec> A = std::vector<Prec>(maxn * ldA);
     int incx = 1;
 
-    generator<prec_t> rg;
+    generator<Prec> rg;
 
     for (char uplo : {'U', 'L'}) {
         for (char diag : {'N', 'U'}) {
             for (char trans : {'C', 'T', 'N'}) {
                 for (int n : {100, 500, 1000, maxn}) {
                     for (kernel_t kernel_type : {USE_BLAS_CALL, UNROLL_1, UNROLL_2}) {
-                        std::cout << "Kernel variant = " << kernel_type_to_str(kernel_type) << ", "
+                        /*std::cout << "Kernel variant = " << kernel_type_to_str(kernel_type) << ", "
                                   << "uplo = " << uplo << " , diag = " << diag << " , trans = " << trans 
-                                  << " , n = " << n << std::endl;
+                                  << " , n = " << n << std::endl;*/
                         // Generate random right-hand side vector, and a triangular matrix.
                         rg.generate_general_matrix(n, 1, x.data(), n);
                         rg.generate_triangular_matrix(uplo, diag, n, A.data(), ldA);
 
-                        bench<prec_t>(kernel_type, uplo, trans, diag, n, A.data(), ldA, x.data(), incx);
+                        auto duration = bench<Prec>(kernel_type, uplo, trans, diag, n, A.data(), ldA, x.data(), incx);
+
+                        out << std::setw(13) << std::left << kernel_type_to_str(kernel_type)
+                            << std::setw( 6) << std::left << uplo
+                            << std::setw( 6) << std::left << diag
+                            << std::setw( 6) << std::left << trans
+                            << std::setw( 8) << std::right << n
+                            << std::setw(12) << std::right << duration.count() << std::endl;
+
                         std::cout << std::endl << std::endl;
                     }
                 }
             }
         }
     }
+
+    out.close();
+}
+
+} // namespace
+
+
+int main(int argc, char **argv)
+{
+    bench<float>("bench_float.csv");
+    bench<double>("bench_double.csv");
+    bench<std::complex<double>>("bench_double_complex.csv");
+    bench<std::complex<float>>("bench_float_complex.csv");
 
     return EXIT_SUCCESS;
 }
--- trsv_internal.hpp
+++ trsv_internal.hpp
@@ -10,14 +10,22 @@ enum kernel_t {
 };
 
 inline std::string kernel_type_to_str(kernel_t kernel_type) {
-    if (kernel_type == USE_BLAS_CALL) return "BLAS Call";
-    else if (kernel_type == UNROLL_1) return "unroll once";
-    else if (kernel_type == UNROLL_2) return "unroll twice";
+    if (kernel_type == USE_BLAS_CALL) return "BLAS";
+    else if (kernel_type == UNROLL_1) return "unroll_once";
+    else if (kernel_type == UNROLL_2) return "unroll_twice";
     else  __builtin_unreachable();
 }
 
 template<typename Prec, kernel_t kernel_type>
 void trsv_selector(char uplo, char trans, char diag, int n,
     const Prec *__restrict__ A, int ldA, Prec *__restrict__ x);
+
+template<typename Prec>
+inline long trsv_flops(int n){
+    if constexpr (is_complex_value<Prec>)
+        return 4 * long(n) * long(n);
+    else
+        return long(n) * long(n);
+}
 
 } // namespace internal
